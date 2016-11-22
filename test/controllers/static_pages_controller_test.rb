11ccda4ed3caf5b432f314ff5fc6029efb910a77
spratiman <spratiman@yahoo.com>
2016-11-22 01:00:54 -0500

    Finish Static home page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "UofT Course Info App"
+  end
+
+  test "should get root" do
+    get static_pages_root
+    assert_response :success
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{{@base_title}}"
   end
 
 end

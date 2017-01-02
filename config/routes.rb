ee33b999cf1e58bb54d00852d35ac1da6ffdf179
Umar Ahmed <umar.ahmed1998@gmail.com>
2017-01-02 03:37:27 -0500

    Integrated most of HTML and CSS for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     delete 'sign_out', :to => 'devise/sessions#destroy', :as => "destroy_user_session_path"
   end
-  root 'static_pages#home'
+  root 'static_pages#index'
   get '/signup', to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

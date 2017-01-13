7e15056ddf7c61185d05fd98ba2ef207e2ede07e
angeline-y <angelineyasodhara@hotmail.com>
2017-01-12 21:12:57 -0500

    Fixed rating save defect

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,10 +4,10 @@ class RatingsController < ApplicationController
     @rating = @course.ratings.find_by(user_id: current_user.id)
     #@rating = Rating.find_by(course_id: :course_id, user_id: current_user.id)
     if @rating.nil?
-    	@course.ratings.create(value: params[:rating][:value], course_id: :course_id, user_id: current_user.id)	
+    	@rating=@course.ratings.create(value: params[:rating][:value], course_id: :course_id, user_id: current_user.id)	
     else
     	@rating.value=params[:rating][:value]
-	end
+	  end
 	@rating.save
 	redirect_to course_path(@course)
   end

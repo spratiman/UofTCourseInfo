5e0f1998aa6738026838938d471192edbe8b0b54
angeliney <angelineyasodhara@hotmail.com>
2017-01-26 20:58:59 -0500

    Added views for ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -8,7 +8,49 @@ class RatingsController < ApplicationController
     else
     	@rating.value=params[:rating][:value]
 	  end
-	@rating.save
-	redirect_to course_path(@course)
+
+	  if @rating.save
+	   redirect_to course_path(@course)
+    else
+      render 'new'
+    end
+  end
+
+  def new
+    @course = Course.find(params[:course_id])
+    @rating = Rating.new
+  end
+
+  def edit
+    @course = Course.find(params[:course_id])
+    @rating = @course.ratings.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:course_id])
+    @rating = Rating.find(params[:id])
+    @rating.value = params[:rating][:value]
+    if @rating.save
+      redirect_to course_path(@course)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @course = Course.find(params[:course_id])
+    @rating = @course.ratings.find(params[:rating][:id])
+    @rating.destroy
+    redirect_to course_path(@course)
+  end
+
+  def show
+    @course = Course.find(params[:course_id])
+    @rating = Rating.find(params[:id])
+  end
+
+  def index
+    @course = Course.find(params[:course_id])
+    @ratings = @course.ratings
   end
 end

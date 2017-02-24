383f39552afbeacbf56f4066382c48ff31721ef1
umar-ahmed <umar.ahmed1998@gmail.com>
2017-02-23 23:37:39 -0500

    Added migration for rating type and fixed add button action bug.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223225601) do
+ActiveRecord::Schema.define(version: 20170224040618) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body",       default: "", null: false
@@ -44,8 +44,9 @@ ActiveRecord::Schema.define(version: 20170223225601) do
     t.integer  "user_id"
     t.integer  "course_id"
     t.integer  "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "rating_type"
     t.index ["course_id"], name: "index_ratings_on_course_id"
     t.index ["user_id", "course_id"], name: "index_ratings_on_user_id_and_course_id", unique: true
     t.index ["user_id"], name: "index_ratings_on_user_id"

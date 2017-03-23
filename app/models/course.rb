9e309a2be214aceee2d50ab181684ae1309f2a26
Angeline <angelineyasodhara@gmail.com>
2017-03-23 00:08:52 -0400

    Added details to course
    Prerequisites, exclusions, breadths as strings

--- app/models/course.rb
+++ app/models/course.rb
@@ -80,12 +80,26 @@ class Course < ApplicationRecord
 		]
 	end
 
-	def self.update_db(code, title, description)
+	def self.update_db(code, title, description, prerequisites, exclusions, breadths)
+		breadth_num_to_val = Hash[1 => "Creative and Cultural Representations",
+															2 => "Thought, Belief, and Behaviour",
+															3 => "Society and Its Institutions",
+															4 => "Living Things and Their Environment",
+															5 => "The Physical and Mathematical Universes"]
+		breadth_string = ""
+		breadths.each do |breadth_num|
+			breadth_string << breadth_num_to_val[breadth_num] + ";"
+		end
+
 		if Course.exists?(:code => code)
 			course = Course.where(code: code)
-			course.update(title: title, description: description)
+			course.update(title: title, description: description,
+										prerequisites: prerequisites, exclusions: exclusions,
+										breadths: breadth_string)
 		else
-			course = Course.new(code: code, title: title, description: description)
+			course = Course.new(code: code, title: title, description: description,
+													prerequisites: prerequisites, exclusions: exclusions,
+													breadths: breadth_string)
 			course.save
 		end
 	end
--- lib/tasks/script_runner.rake
+++ lib/tasks/script_runner.rake
@@ -1,6 +1,6 @@
 desc "Runs an external ruby script"
 task :update_course => :environment do
-    filepath = Rails.root.join('lib', 'assets', 'fetch_course.rb')
+    filepath = Rails.root.join('lib', 'assets', 'update_course.rb')
 	output = `ruby #{filepath}`
 	puts output
 end
